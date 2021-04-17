b772cf88ce13eb930cae94ecddca4f8a0a4684a1
guhankesav <47454368+guhankesav@users.noreply.github.com>
2021-04-17 23:40:36 +0530

    Create assign-cookies.py

---
+++ assign-cookies.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        j=0
+        count=0
+        s.sort()
+        g.sort()
+        for i in s:
+            if(len(g)==0):
+                break
+            if i >=g[j]:
+                g.pop(0)
+                count+=1
+        return count
+                
