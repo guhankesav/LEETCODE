13c0aca3fabe0de0aaa343376e5bba8cc28d9c4a
Guhan K <47454368+guhankesav@users.noreply.github.com>
2022-04-26 19:05:26 +0530

    Time: 114 ms (13.50%), Space: 14.1 MB (93.43%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.py
+++ 1629-slowest-key/1629-slowest-key.py
@@ -1,5 +1,3 @@
 class Solution:
-    def slowestKey(self, times, keys):
-        durations = [times[i]-times[i-1] if i>0 else times[i] for i in range(len(times))]
-        slowestKeys = [keys[i] for i in range(len(durations)) if durations[i] == max(durations)]
-        return max(slowestKeys)
+    def slowestKey(self, t, k):
+        return max(zip(map(sub,t,[0]+t),k))[1]
