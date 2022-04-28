40e23dd203265ee4a4cd39543384f5627f901380
Guhan K <47454368+guhankesav@users.noreply.github.com>
2022-04-28 15:39:31 +0530

    Time: 62 ms (89.13%), Space: 15.3 MB (23.40%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -6,9 +6,9 @@ class Solution:
         for i in range(len(prices)-1):
             meh = prices[i+1] - prices[i]
             L.append(meh)
-            if meh > 0:
-                msf += meh
-            elif meh < 0:
-                meh = 0
+            # if meh > 0:
+            #     msf += meh
+            # elif meh < 0:
+            #     meh = 0
         print(L)
         return sum([num for num in L if num >= 0])
