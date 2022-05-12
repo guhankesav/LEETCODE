3c6c21eafe0e301bef672259b9fa6c89348d5c80
Guhan K <47454368+guhankesav@users.noreply.github.com>
2022-05-12 23:47:40 +0530

    Time: 1385 ms (59.90%), Space: 15.3 MB (10.66%) - LeetHub

---
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+        
+        for x in [tops[0], bottoms[0]]:
+            if all(x in domino for domino in zip(tops, bottoms)):
+                return len(tops) - max(tops.count(x), bottoms.count(x))
+        
+        return -1
