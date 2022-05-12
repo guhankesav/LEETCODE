9089dedf3ebbb9ac4f1dd56fc9f74ddf74c15983
Guhan K <47454368+guhankesav@users.noreply.github.com>
2022-05-12 23:48:45 +0530

    Time: 1777 ms (24.92%), Space: 15.1 MB (75.92%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -1,8 +1,37 @@
 class Solution:
-    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
-        
-        for x in [tops[0], bottoms[0]]:
-            if all(x in domino for domino in zip(tops, bottoms)):
-                return len(tops) - max(tops.count(x), bottoms.count(x))
-        
-        return -1
+	def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+
+		frequency_tops = {1:0,2:0,3:0,4:0,5:0,6:0}
+
+		for i in tops:
+			if i not in frequency_tops:
+				frequency_tops[i] = 1
+			else:
+				frequency_tops[i] = frequency_tops[i] + 1
+
+		frequency_bottoms = {1:0,2:0,3:0,4:0,5:0,6:0}
+
+		for i in bottoms:
+			if i not in frequency_bottoms:
+				frequency_bottoms[i] = 1
+			else:
+				frequency_bottoms[i] = frequency_bottoms[i] + 1
+
+		swap_number = 0
+
+		for i in range(1,7):
+			if frequency_tops[i] + frequency_bottoms[i] >= len(tops):
+				swap_number = i
+
+		if swap_number == 0:
+			return -1
+
+		min_num1 = len(tops)-frequency_tops[swap_number]
+		min_num2 = len(bottoms)-frequency_bottoms[swap_number]
+
+
+		for i in range(len(tops)):
+			if swap_number not in [tops[i],bottoms[i]]:
+				return -1
+
+		return min(min_num1,min_num2)
