b7cbe86ae44621f2d5d2d31934f474014ba2ee72
Guhan K <47454368+guhankesav@users.noreply.github.com>
2022-05-12 23:49:51 +0530

    Time: 1520 ms (46.61%), Space: 14.9 MB (94.61%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -1,37 +1,38 @@
 class Solution:
-	def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
-
-		frequency_tops = {1:0,2:0,3:0,4:0,5:0,6:0}
-
-		for i in tops:
-			if i not in frequency_tops:
-				frequency_tops[i] = 1
-			else:
-				frequency_tops[i] = frequency_tops[i] + 1
-
-		frequency_bottoms = {1:0,2:0,3:0,4:0,5:0,6:0}
-
-		for i in bottoms:
-			if i not in frequency_bottoms:
-				frequency_bottoms[i] = 1
-			else:
-				frequency_bottoms[i] = frequency_bottoms[i] + 1
-
-		swap_number = 0
-
-		for i in range(1,7):
-			if frequency_tops[i] + frequency_bottoms[i] >= len(tops):
-				swap_number = i
-
-		if swap_number == 0:
-			return -1
-
-		min_num1 = len(tops)-frequency_tops[swap_number]
-		min_num2 = len(bottoms)-frequency_bottoms[swap_number]
-
-
-		for i in range(len(tops)):
-			if swap_number not in [tops[i],bottoms[i]]:
-				return -1
-
-		return min(min_num1,min_num2)
+    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+        
+        x=tops[0]
+        y=bottoms[0]
+        res=-1
+        fx=1
+        fy=1
+        for i in range(len(tops)):
+            if tops[i]==x or bottoms[i]==x:
+                continue
+            else:
+                fx=0
+                break
+        
+        for i in range(len(tops)):
+            if tops[i]==y or bottoms[i]==y:
+                continue
+            else:
+                fy=0
+                break
+        
+        if fx==0 and fy==0:
+            return -1
+        
+        res=100000
+        
+        if fx==1:
+            a=tops.count(x)
+            b=bottoms.count(x)
+            res=len(tops)-(max(a,b))
+        if fy==1:
+            a=tops.count(y)
+            b=bottoms.count(y)
+            res=min(res,len(tops)-(max(a,b)))
+            
+        
+        return res
