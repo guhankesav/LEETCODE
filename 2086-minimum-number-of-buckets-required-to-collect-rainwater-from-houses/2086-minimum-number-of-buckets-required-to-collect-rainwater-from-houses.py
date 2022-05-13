ce155fc577cdc52dd6f7394cb2356fce452011bd
Guhan K <47454368+guhankesav@users.noreply.github.com>
2022-05-13 19:58:21 +0530

    Time: 33 ms (98.25%), Space: 14.9 MB (99.56%) - LeetHub

---
+++ 2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses/2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses.py
@@ -0,0 +1,4 @@
+class Solution:
+    def minimumBuckets(self, s: str) -> int:
+        return -1 if 'HHH' in s or s[:2] == 'HH' or s[-2:] == 'HH' or s == 'H' else s.count('H') - s.count('H.H')
+        
